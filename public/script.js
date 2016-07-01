0fd8723544f2cfc8f64aad929a54553d17d61c5f
Mina <wilhelmina.jane@gmail.com>
2016-07-01 19:22:40 +0100

    semi-complete code

---
+++ public/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $.ajax({
+        url: '/get-posts',
+        dataType: 'json',
+        method: 'GET',
+        success: function(data) {
+            console.log("DATA", data);
+            for (var blogPost in data) {
+                var postDiv         = document.createElement('div');
+                var postText        = document.createElement('p');
+                var thumbnail       = document.createElement('img');
+                var postContainer   = document.getElementsByClassName('post-container')[0];
+
+                thumbnail.src = "./img/logo2.png";
+                thumbnail.className = "thumbnail";
+                postText.innerHTML = data[blogPost];
+                postDiv.className = "post";
+
+                postDiv.appendChild(thumbnail);
+                postDiv.appendChild(postText);
+                postContainer.appendChild(postDiv);
+
+            }
+        },
+        error: function(error){
+            console.error("ERROR", error);
+        }
+    });
+});
---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var fs = require('fs');
+
+
+app.use(express.static('public'));
+app.use(express.static('data'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/get-posts', function (req, res) {
+
+    res.sendFile(__dirname + '/data/posts.json');
+});
+
+app.post('/create-post', function (req, res) {
+
+    fs.readFile(__dirname + '/data/posts.json', function (error, file) {
+
+        var parsedFile = JSON.parse(file);
+        console.log(parsedFile);
+        parsedFile[Date.now()] = req.body.blogpost;
+        var stringifiedFile = JSON.stringify(parsedFile, null, 4);
+        console.log(stringifiedFile);
+        fs.writeFile(__dirname + '/data/posts.json', stringifiedFile, function (error) {
+
+            if (error) {
+                console.error(error);
+            }
+            res.redirect('/');
+        });
+    });
+});
+
+app.listen(3000, function () {
+    console.log('Server is listening on port 3000. Ready to accept requests!');
+});
